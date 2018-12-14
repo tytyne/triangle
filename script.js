a94bbb4104fb127b843bbab022c7dd5fe1f73717
tytyne <dusaflora2@gmail.com>
2018-12-14 14:05:31 +0200

    commit message

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
-function check(){
-    var firstvalue=parseInt(document.getElementById('one').value);
-    var secondvalue=parseInt(document.getElementById('two').value);
-    var thirdvalue=parseInt(document.getElementById('three').value);
-    var array=[firstvalue,secondvalue,thirdvalue];
+function check() {
+    var firstvalue = parseInt(document.getElementById('one').value);
+    var secondvalue = parseInt(document.getElementById('two').value);
+    var thirdvalue = parseInt(document.getElementById('three').value);
+    var array = [firstvalue, secondvalue, thirdvalue];
     var text;
-    if(firstvalue <= 0|| secondvalue <= 0 || thirdvalue <=0 ){
-        text="Undefined"
-    }
-    else if (
+    if (firstvalue <= 0 || secondvalue <= 0 || thirdvalue <= 0) {
+        text = "Undefined"
+    } else if (
         (firstvalue + secondvalue <= thirdvalue &&
-          secondvalue + firstvalue <= thirdvalue)||
-            (firstvalue + thirdvalue <= secondvalue &&
+            secondvalue + firstvalue <= thirdvalue) ||
+        (firstvalue + thirdvalue <= secondvalue &&
             thirdvalue + firstvalue <= secondvalue) ||
-            (secondvalue + thirdvalue <= firstvalue && thirdvalue + secondvalue <= firstvalue)
-         ) {
+        (secondvalue + thirdvalue <= firstvalue && thirdvalue + secondvalue <= firstvalue)
+    ) {
         text = "Not a Triangle.";
-      }
+    }
     // Not a triangle
-    else if(firstvalue === secondvalue && 
-        firstvalue===thirdvalue &&
-        secondvalue === thirdvalue){
+    else if (firstvalue === secondvalue &&
+        firstvalue === thirdvalue &&
+        secondvalue === thirdvalue) {
         text = "Equilaterial Triangle.";
     }
-     //Equivalent Triangle
-    
-    else if(firstvalue===secondvalue || 
-        firstvalue===thirdvalue || 
-        secondvalue===thirdvalue) {
-        text="Isosceles Triangle.";
-    } 
+    //Equivalent Triangle
+    else if (firstvalue === secondvalue ||
+        firstvalue === thirdvalue ||
+        secondvalue === thirdvalue) {
+        text = "Isosceles Triangle.";
+    }
     //Isoscele Triangle
-    else if(firstvalue!==secondvalue &&
-         firstvalue!==thirdvalue &&
-         secondvalue!==thirdvalue){
-        text="Scalene Triangle."
+    else if (firstvalue !== secondvalue &&
+        firstvalue !== thirdvalue &&
+        secondvalue !== thirdvalue) {
+        text = "Scalene Triangle."
     }
     //Scalene Triangle
-    
-    else{
+    else {
         text = "Error.";
     }
-    
-    document.getElementById('demo').innerHTML="The side of trianges are["+array+"]"+" is ";
+
+    document.getElementById('demo').innerHTML = "The side of trianges are[" + array + "]" + " is <br>";
     document.getElementById('demo').innerHTML += text;
 
 }
-
